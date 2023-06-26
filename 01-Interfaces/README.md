97863b188f2bd35c80442579ea9969f9168f9a25
Diana <dvelasquez@frba.utn.edu.ar>
2023-06-26 15:47:26 -0300

    Entrega de TP1

---
+++ 01-Interfaces/README.md
@@ -0,0 +1,5 @@
+##Trabajo Práctico N°1##
+
+El presente trabajo práctico consiste en desarrollar un programa que haga uso de interfaces y módulos, comprendiendo como intervienen los archivos fuentes en el proceso BUILD, así como tambien el uso del Makefile para diseñar parámetros que pertenecen al mismo.
+
+Se desorrolló el código con los puntos extras dados en la consigna y se integró a los archivos fuente.
---
+++ 01-Interfaces/conversion.h
@@ -0,0 +1,17 @@
+const int LOWER_F = 0; /*límite inferior de fahr*/
+const int UPPER_F = 300; /*límite superior de fahr*/
+const int STEP_F = 20; /*incremento de fahr*/
+
+const int LOWER_C = -10; /*límite inferior de celsius*/
+const int UPPER_C = 150; /*límite superior de celsius*/
+const int STEP_C = 10; /*incremento de celsius*/
+
+//Se definen las variables globales como constantes según restricción de la consigna.
+
+float fahr(float celsius);
+float celsius(float farh);
+void printTablas();
+void printTablaCelsius();
+void printTablaFahr();
+void printFilas(int base,int top,int inc,float funcion(float));
+void printFila(float deg,float degConverted);
